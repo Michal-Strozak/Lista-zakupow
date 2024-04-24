16b71f92589d7fc5b702ba3e79775904ea5e582b
Michał Stróżak <168005303+Michal-Strozak@users.noreply.github.com>
2024-04-24 18:35:12 +0200

    Product.java

---
+++ Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    String name;
+    String category;
+    double quantity;
+    String unit;
+
+    Product(String name, String category, double quantity, String unit) {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+}
